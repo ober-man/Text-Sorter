f5ea19d05354befb65b63c879adeb82ed590dc77
ober-man <71032009+ober-man@users.noreply.github.com>
2020-09-09 22:00:29 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <math.h>
+#include "sort_functions.h"
+
+const long MAXSYMB = 10e6; ///max number of symbols in file
+const int MAXLINES = 10e6; ///max number of lines in file
+
+/** Struct "line" contains pointer to the start of the line end it's length **/
+struct line
+{
+    char* pointer;
+    int len;
+};
+
+int main()
+{
+
+    /** Open files **/
+    FILE* input = fopen("input.txt", "r");
+    FILE* output = fopen("result.txt", "w");
+
+    /** File reading **/
+    long symb = CountSymbols(input);
+    char* array = FileToArray(input, symb);
+    int lin = CountLines(array, symb);
+    line* str = ArrayToStruct(array, lin, symb);
+
+    /** Sorting of the file by alphabet **/
+    qsort(str, lin, sizeof(line), Compar);
+    fputs("Sorting by alphabet\n", output);
+    StructToFile(str, lin, output);
+    fputs("\n", output);
+
+    /** Sorting of the file by rhymes **/
+    qsort(str, lin, sizeof(line), ComparEnd);
+    fputs("Sorting by rhymes\n", output);
+    StructToFile(str, lin, output);
+    fputs("\n", output);
+
+    /** Origin text **/
+    fputs("Origin text\n", output);
+    ArrayToFile(array, symb, output);
+    fputs("\n", output);
+
+    fclose(output);
+    fclose(input);
+    free(array);
+    free(str);
+    return 0;
+}
